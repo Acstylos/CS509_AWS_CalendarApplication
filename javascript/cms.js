23aa6e112b26c05fe2e934700e53fb074c7b2a7d
acstylos <a.stylos983@gmail.com>
2018-10-26 15:01:35 -0400

    initial commit of basic frontend

---
+++ javascript/cms.js
@@ -0,0 +1,10 @@
+function createDayCard(event) {
+    var $this = $(this);
+    event.preventDefualt();
+    document.getElementById(which).innerHTML = 
+    $('#'+which).load('test.html');
+}
+
+function populateCalendar() {
+
+}
