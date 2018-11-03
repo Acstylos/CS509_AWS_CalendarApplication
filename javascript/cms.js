3c0849665c261f696c6980a61f1f8ef4c2fee080
acstylos <a.stylos983@gmail.com>
2018-11-03 19:00:53 -0400

    skeleton the javascript calls

--- javascript/cms.js
+++ javascript/cms.js
@@ -1,36 +1,64 @@
-function loadCalendarByName(e) {
-    var form = document.loadByNameForm;
+// API-Endpoints
+const apiUrl = "https://x325bb0xrc.execute-api.us-east-2.amazonaws.com/Alpha/";
+const calendarsEndpoint = "calendars/";
+const timeslotsEndpoint = "timeslots/";
+
+
+function getCalendarByName(event) {
+    var form = document.selectCalendar;
     var name = form.calendarNameSelection.value;
 
-    var requestLocation = "https://x325bb0xrc.execute-api.us-east-2.amazonaws.com/Alpha/calendars/";
-    var req = requestLocation + name;
+    var request = apiUrl + calendarsEndpoint + name;
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", req, true);
-
-    xhr.onloadend = function () {
-        alert(xhr.responseText);
-        if (xhr.readyState == XMLHttpRequest.Done) {
-            alert(xhr.responseText);
-        }
-    }
-
+    xhr.open("GET", request, true);
+    xhr.onloadend = displayCalendar(xhr.responseText);
     xhr.send(null);
 }
 
-function fillOutCalendar() {
-    
-    
+function deleteCalendarByName(event) {
+    var form = document.selectCalendar;
+    var name = form.calendarNameSelection.value;
+
+    var request = apiUrl + calendarsEndpoint + name;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("DELETE", request, true);
+    xhr.onloadend = deleteCalendar(xhr.responseText);
+    xhr.send(null);
 }
 
-/*
-function createDayCard(event) {
-    var $this = $(this);
-    event.preventDefualt();
-    document.getElementById(which).innerHTML = 
-    $('#'+which).load('test.html');
+function getCalendarNames(event) {
+    var request = apiUrl + calendarsEndpoint;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", request, true);
+    xhr.onloadend = updateSelectCalendarDropdown(xhr.responseText);
+    xhr.send(null);
 }
 
-function populateCalendar() {
+function postCreateCalendar(event) {
+    var form = document.createNewCalendar;
+    // get form data
+    var json = "{}";
+    // JSON-ify the data
 
-}*/
+    var request = apiUrl + calendarsEndpoint;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", request, true);
+    xhr.onloadend = displayCalendar(xhr.responseText);
+    xhr.send(json);
+}
+
+function displayCalendar(data) {
+    alert(data);
+}
+
+function updateSelectCalendarDropdown(data) {
+    alert(data);
+}
+
+function deleteCalendar(data) {
+    alert(data);
+}
