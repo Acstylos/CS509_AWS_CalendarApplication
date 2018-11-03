ae83cd42570f8d2d320ee7c886336e9be7b7e18e
Alexander Stylos <astylos@highresbio.com>
2018-11-03 18:11:58 -0400

    working with loadCalendarByName lambda

--- javascript/cms.js
+++ javascript/cms.js
@@ -1,3 +1,29 @@
+function loadCalendarByName(e) {
+    var form = document.loadByNameForm;
+    var name = form.calendarNameSelection.value;
+
+    var requestLocation = "https://x325bb0xrc.execute-api.us-east-2.amazonaws.com/Alpha/calendars/";
+    var req = requestLocation + name;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", req, true);
+
+    xhr.onloadend = function () {
+        alert(xhr.responseText);
+        if (xhr.readyState == XMLHttpRequest.Done) {
+            alert(xhr.responseText);
+        }
+    }
+
+    xhr.send(null);
+}
+
+function fillOutCalendar() {
+    
+    
+}
+
+/*
 function createDayCard(event) {
     var $this = $(this);
     event.preventDefualt();
@@ -7,4 +33,4 @@ function createDayCard(event) {
 
 function populateCalendar() {
 
-}
+}*/
